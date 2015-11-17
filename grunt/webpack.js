c53f3091853715834a1de12d73bf7e92ce87df1b
Leonardo Balter <leonardo.balter@gmail.com>
2015-11-16 19:35:48 -0500

    webpack build use process.env.APP_ENV

--- grunt/webpack.js
+++ grunt/webpack.js
@@ -38,10 +38,12 @@ module.exports = function( grunt ) {
 			]
 		},
 		build: {
-			watch: false,
+
 			debug: false,
-			cache: true,
 			plugins: [
+				new webpack.DefinePlugin({
+          APP_ENV: JSON.stringify( process.env.APP_ENV || "development" ),
+        }),
 				new webpack.optimize.DedupePlugin(),
 				new webpack.optimize.UglifyJsPlugin( { compress: { warnings: false } } )
 			]
