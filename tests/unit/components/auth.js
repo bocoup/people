be7a7617e383fcaf7ea300a10b5442db56f9d0ee
Leonardo Balter <leonardo.balter@gmail.com>
2015-11-20 12:23:06 -0500

    Tests example, using normal rendering and shallow rendering

--- tests/unit/components/auth.js
+++ tests/unit/components/auth.js
@@ -1,6 +1,7 @@
 import Auth from "../../../src/components/auth";
 import TestUtils from "react-addons-test-utils";
 import React from "react"; // eslint-disable-line no-unused-vars
+import renderer from "../helpers/shallow-renderer";
 
 const { module, test } = QUnit;
 
@@ -8,17 +9,25 @@ module( "Components - Auth", function() {
 	this.logIn = () => 43;
 	this.logOut = () => 44;
 
-	test( "loading", function( assert ) {
-		assert.equal( typeof Auth, "function" );
-
+	test( "log in button", function( assert ) {
 		let auth = TestUtils.renderIntoDocument(
-			<Auth token="42" logIn={ this.logIn } logOut={ this.logOut } />
+			<Auth token="" logIn={ this.logIn } logOut={ this.logOut } />
 		);
 
-		console.log( auth );
+		let button = TestUtils.findRenderedDOMComponentWithTag( auth, "button" );
 
-		// let button = TestUtils.findRenderedDOMComponentWithTag( auth, "button" );
+		assert.equal( button.textContent, "Log In" );
+	} );
 
-		// assert.equal( button.getDOMNode().textContent, "login" );
+	test( "log in button - using a shallow renderer", assert => {
+		renderer.render(
+			<Auth token="" logIn={ this.logIn } logOut={ this.logOut } />
+		);
+
+		let auth = renderer.getRenderOutput();
+
+		assert.equal( auth.type, "div" );
+		assert.equal( auth.props.children.type, "button" );
+		assert.equal( auth.props.children.props.children, "Log In" );
 	} );
 } );
