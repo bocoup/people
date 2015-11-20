674fed44736655441d2d75469ac61751d0f76dcc
Leonardo Balter <leonardo.balter@gmail.com>
2015-11-20 11:48:55 -0500

    First draft of testing a React component

--- tests/unit/components/auth.js
+++ tests/unit/components/auth.js
@@ -1,7 +1,24 @@
 import Auth from "../../../src/components/auth";
+import TestUtils from "react-addons-test-utils";
+import React from "react"; // eslint-disable-line no-unused-vars
 
-QUnit.module( "Components - Auth", () => {
-	QUnit.test( "loading", assert => {
-		assert.equal( typeof Auth, "object" );
+const { module, test } = QUnit;
+
+module( "Components - Auth", function() {
+	this.logIn = () => 43;
+	this.logOut = () => 44;
+
+	test( "loading", function( assert ) {
+		assert.equal( typeof Auth, "function" );
+
+		let auth = TestUtils.renderIntoDocument(
+			<Auth token="42" logIn={ this.logIn } logOut={ this.logOut } />
+		);
+
+		console.log( auth );
+
+		// let button = TestUtils.findRenderedDOMComponentWithTag( auth, "button" );
+
+		// assert.equal( button.getDOMNode().textContent, "login" );
 	} );
 } );
