b98e6d2a4fe407709ed6c949cbbfc2f231a2169d
Jenn Schiffer <jenn@dotbiz.info>
2016-02-22 10:51:01 -0500

    add react router oauth module

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import App from "./components/app";
 import List from "./components/list";
 import Profile from "./components/profile";
 import NoMatch from "./components/no-match";
+import auth from './auth';
 
 render( (
 	<Router history={ createHistory() }>
 		<Route path="/" component={ App }>
-			<IndexRoute component={ List } />
-			<Route path="profile/:slug" component={ Profile } />
-
+			<IndexRoute component={ List } onEnter={ auth.requireNoAuth } />
+			<Route path="profile/:slug" component={ Profile } onEnter={ auth.requireNoAuth } />
 			<Redirect from="profile" to="/" />
 			<Route path="*" component={ NoMatch } />
 		</Route>
