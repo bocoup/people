5b73ec2900ea264bb976819c347ee2c9e4e2f593
Jenn Schiffer <jenn@dotbiz.info>
2016-02-22 12:52:57 -0500

    add login component

--- src/index.js
+++ src/index.js
@@ -8,11 +8,22 @@ import Profile from "./components/profile";
 import NoMatch from "./components/no-match";
 import auth from "./util/auth";
 
+const Login = ( { location: { state } } ) => {
+	const redirectTo = state && state.nextPathname;
+	return (
+		<div>
+			<p>You must log in to use this app!</p>
+			<a href={auth.authUrl( redirectTo )}>Log In</a>
+		</div>
+	);
+};
+
 render( (
 	<Router history={ createHistory() }>
-		<Route path="/" component={ App }>
-			<IndexRoute component={ List } onEnter={ auth.requireNoAuth } />
-			<Route path="profile/:slug" component={ Profile } onEnter={ auth.requireNoAuth } />
+		<Route path="/login" component={ Login } onEnter={ auth.requireAuth } />
+		<Route path="/" component={ App } onEnter={ auth.requireAuth }>
+			<IndexRoute component={ List } />
+			<Route path="profile/:slug" component={ Profile } />
 			<Redirect from="profile" to="/" />
 			<Route path="*" component={ NoMatch } />
 		</Route>
